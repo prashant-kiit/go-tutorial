e7fef3ea7dc1ad04e324664bbef32693cc8a34a4
prashant-m4 <prashantsingh090798@gmail.com>
2025-08-20 23:37:27 +0530

    feat: added float (double) exmaple for bit size and accuracy

--- src/tutorial-2/main.go
+++ src/tutorial-2/main.go
@@ -5,7 +5,13 @@ import "fmt"
 func main() {
 	var age int16 = 30;
 	var points uint16 = 1000;
-	age = age + 400000000; // run time overflow error
+	// age = age + 400000000; // run time overflow error
 	points = points - 4000; // no runtime overflow error 
+
+	var cost1 float32 = 12.89788888888; // less accurate
+	var cost2 float64 = 12.89788888888; // more accurate
+	// var cost2 double = 12.89788888888; // double do not exists
+
 	fmt.Println("Hello Prashant is ", age, points);
+	fmt.Println(cost1, cost2);
 }
