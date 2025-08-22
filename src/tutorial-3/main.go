cf14091671f2704956caadad03ea6f0e9dc4f853
prashant-m4 <prashantsingh090798@gmail.com>
2025-08-22 22:00:27 +0530

    feat: added another function with arguments and return types

--- src/tutorial-3/main.go
+++ src/tutorial-3/main.go
@@ -4,10 +4,18 @@ import "fmt"
 
 func main() {
 	var result float64 = divide(11, 2)
+	var quotient, rem int = divideByInt(11, 2)
 	fmt.Println(result)
+	fmt.Println(quotient, rem)
 }
 
 func divide(numerator float64, denominator float64) float64 {
-	var result float64 = numerator / denominator;
+	var result float64 = numerator / denominator
 	return result
+}
+
+func divideByInt(numerator int, denominator int) (int, int) {
+	var quotient int = numerator / denominator
+	var rem int = numerator % denominator
+	return quotient, rem
 }
