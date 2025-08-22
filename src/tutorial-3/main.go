9e64f6731109dd5e7e54d98897d866e48c9e1156
prashant-m4 <prashantsingh090798@gmail.com>
2025-08-22 22:10:36 +0530

    feat: added error handler

--- src/tutorial-3/main.go
+++ src/tutorial-3/main.go
@@ -1,21 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 func main() {
-	var result float64 = divide(11, 2)
-	var quotient, rem int = divideByInt(11, 2)
+	var result, err1 = divide(11, 0)
+	var quotient, rem, err2 = divideByInt(11, 2)
+	if err1 != nil {
+		fmt.Println(err1.Error())
+	}
 	fmt.Println(result)
+	if err2 != nil {
+		fmt.Println(err2.Error())
+	}
 	fmt.Println(quotient, rem)
 }
 
-func divide(numerator float64, denominator float64) float64 {
+func divide(numerator float64, denominator float64) (float64, error) {
+	var err error
+	if denominator == 0 {
+		err = errors.New("the denominator is zero")
+		return 0, err
+	}
 	var result float64 = numerator / denominator
-	return result
+	return result, err
 }
 
-func divideByInt(numerator int, denominator int) (int, int) {
+func divideByInt(numerator int, denominator int) (int, int, error) {
+	var err error
+	if denominator == 0 {
+		err = errors.New("the denominator is zero")
+		return 0, 0, err
+	}
 	var quotient int = numerator / denominator
 	var rem int = numerator % denominator
-	return quotient, rem
+	return quotient, rem, err
 }
