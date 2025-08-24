c5ac4008d523bad77d09a608e7929c919995cdbb
prashant-m4 <prashantsingh090798@gmail.com>
2025-08-24 16:03:22 +0530

    feat: added array code example

---
+++ src/tutorial-4/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var arr1 [3]int64 // default {0,0,0}
+	fmt.Println(arr1)
+	var arr2 [3]byte // default {0,0,0}
+	fmt.Println(arr2)
+	var arr3 [3]rune // default {0,0,0}
+	fmt.Println(arr3)
+	var arr4 [3]bool // default {0,0,0}
+	fmt.Println(arr4)
+	var arr5 [3]string // default {'','',''}
+	fmt.Println(len(arr5)) // 3
+}
