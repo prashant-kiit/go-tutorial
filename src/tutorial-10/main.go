f55388551d7bff877bc6aefb62862b13c13a0049
prashant-m4 <prashantsingh090798@gmail.com>
2025-09-17 00:02:22 +0530

    feat: added code for locks and waitgroups (latch)

--- src/tutorial-10/main.go
+++ src/tutorial-10/main.go
@@ -2,30 +2,43 @@ package main
 
 import (
 	"fmt"
-	"runtime"
+	// "runtime"
 	"time"
+	"sync"
 )
 
-func printMessage(msg string, t time.Duration) {
-	for i := 1; i <= 3; i++ {
+var count int = 0
+
+func printMessage(msg string, t time.Duration, wg *sync.WaitGroup, m *sync.Mutex) {
+	for i := 1; i <= 1000000; i++ {
+		m.Lock()
+		count++
+		m.Unlock()
 		fmt.Println(msg, i)
 		time.Sleep(t * time.Millisecond)
 	}
+
+	wg.Done()
 }
 
 func foo1() {
 	// Run as goroutine
-	go printMessage("Hello from Goroutine 1", 5000)
-	go printMessage("Hello from Goroutine 2", 10000)
+	var wg sync.WaitGroup
+	var m = sync.Mutex{}
+	wg.Add(2)
+	go printMessage("Hello from Goroutine 1", 0, &wg, &m)
+	go printMessage("Hello from Goroutine 2", 0, &wg, &m)
+	wg.Wait()
 
 	// Main function also runs concurrently
-	printMessage("Hello from Main", 10)
+	// printMessage("Hello from Main", 10)
+	fmt.Println("All goroutines completed", count)
 }
 
 func main() {
 	foo1()
-	fmt.Println("CPUs:", runtime.NumCPU())
-	fmt.Println("GOMAXPROCS:", runtime.GOMAXPROCS(2)) 
+	// fmt.Println("CPUs:", runtime.NumCPU())
+	// fmt.Println("GOMAXPROCS:", runtime.GOMAXPROCS(2)) 
 }
 
 // Where do Goroutines fit?
