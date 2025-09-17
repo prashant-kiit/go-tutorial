81b3f8a517fad7900e12aae016939059bf3beb05
prashant-m4 <prashantsingh090798@gmail.com>
2025-09-17 23:50:44 +0530

    feat: added rwlock

--- src/tutorial-10/main.go
+++ src/tutorial-10/main.go
@@ -21,18 +21,30 @@ func printMessage(msg string, t time.Duration, wg *sync.WaitGroup, m *sync.Mutex
 	wg.Done()
 }
 
+func readMessage(msg string, t time.Duration, wg *sync.WaitGroup, m *sync.RWMutex) {
+	for i := 1; i <= 1000000; i++ {
+		m.RLock()
+		fmt.Println(msg, i, count)
+		time.Sleep(t * time.Millisecond)
+		m.RUnlock()
+	}
+	wg.Done()
+}
+
 func foo1() {
 	// Run as goroutine
 	var wg sync.WaitGroup
-	var m = sync.Mutex{}
-	wg.Add(2)
-	go printMessage("Hello from Goroutine 1", 0, &wg, &m)
-	go printMessage("Hello from Goroutine 2", 0, &wg, &m)
+	var m1 = sync.Mutex{}
+	var m2 = sync.RWMutex{}
+	wg.Add(4)
+	go printMessage("Hello from Goroutine 1", 0, &wg, &m1)
+	go printMessage("Hello from Goroutine 2", 0, &wg, &m1)
+	go readMessage("Hello from Goroutine 3", 0, &wg, &m2)
+	go readMessage("Hello from Goroutine 4", 0, &wg, &m2)
 	wg.Wait()
-
+	fmt.Println("All goroutines completed", count)
 	// Main function also runs concurrently
 	// printMessage("Hello from Main", 10)
-	fmt.Println("All goroutines completed", count)
 }
 
 func main() {
